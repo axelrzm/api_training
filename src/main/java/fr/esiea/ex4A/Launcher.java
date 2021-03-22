383cf2a47830220df61e7ecd98a7a3db7075d7cc
Axel Razafimahefa <axel.raza12@gmail.com>
2021-03-22 09:13:36 +0100

    Add Agify API

--- src/main/java/fr/esiea/ex4A/Launcher.java
+++ src/main/java/fr/esiea/ex4A/Launcher.java
@@ -1,10 +1,22 @@
 package fr.esiea.ex4A;
 
+import fr.esiea.ex4A.service.AgifyClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import retrofit2.Retrofit;
 
 @SpringBootApplication
 public class Launcher {
+
+    @Bean
+    AgifyClient agifyClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.agify.io")
+            .build();
+
+        return retrofit.create(AgifyClient.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Launcher.class, args);
---
+++ src/main/java/fr/esiea/ex4A/data/User.java
@@ -0,0 +1,8 @@
+package fr.esiea.ex4A.data;
+
+public class User {
+    public String name;
+    public String age;
+    public String count;
+    public String country_id;
+}

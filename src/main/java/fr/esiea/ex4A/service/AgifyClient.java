ecae90d3f9e04150cdc3963d06eece9487513107
Axel Razafimahefa <axel.raza12@gmail.com>
2021-03-25 23:28:31 +0100

    Add Services and refactoring code

--- src/main/java/fr/esiea/ex4A/service/AgifyClient.java
+++ src/main/java/fr/esiea/ex4A/service/AgifyClient.java
@@ -1,13 +1,11 @@
 package fr.esiea.ex4A.service;
 
-import fr.esiea.ex4A.data.User;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-import java.util.List;
-
 public interface AgifyClient {
     @GET("/?")
-    Call<List<User>> getUser(@Query("name") String name, @Query("country_id") String country_id);
+    Call<ResponseBody> getUser(@Query("name") String name, @Query("country_id") String country_id);
 }
